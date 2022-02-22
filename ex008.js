52b126038c98f6dea3a07504456f2994be4a3f1c
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2022-02-22 00:38:22 -0300

    update

---
+++ ex008.js
@@ -0,0 +1,5 @@
+var vel = 60
+console.log(`A velocidade eh ${vel}km/h`)
+if(vel > 60){
+    console.log(`Voce esta acima da velocidade permitida`)
+}
---
+++ ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+
+if(pais == 'Brasil')
+    console.log(`Voce eh brasileiro`)
+
+else
+    console.log(`Voce eh estrangeiro`)
