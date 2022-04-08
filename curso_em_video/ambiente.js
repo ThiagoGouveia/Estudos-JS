335fa16b04e9b2ca3b4f345c66ee3e41ae3a497e
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2022-04-08 17:34:48 -0300

    update

---
+++ curso_em_video/ambiente.js
@@ -0,0 +1,17 @@
+// var c = 1 
+// do{
+//     console.log(`Passo ${c}`)
+//     c++ 
+// }while (c <= 10)
+
+
+// var c = 1 
+// while (c <= 10){
+//     console.log(`Passo ${c}`)
+//     c++ 
+// }
+
+console.log('vai comecar')
+for(var c = 1; c <= 10; c++){
+    console.log(`Passo ${c}`)
+}
